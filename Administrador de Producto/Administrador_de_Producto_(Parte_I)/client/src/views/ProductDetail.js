61f0866b35bbe722f7a7ff9d0714ea2c35578003
unknown <rociooviedo26@icloud.com>
2023-01-31 03:46:08 -0300

    Administrador de Producto Parte II

--- Administrador de Producto/Administrador_de_Producto_(Parte_I)/client/src/views/ProductDetail.js
+++ Administrador de Producto/Administrador_de_Producto_(Parte_I)/client/src/views/ProductDetail.js
@@ -1,10 +1,13 @@
 
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import { useParams } from 'react-router-dom'
+
 export default props => {
+    const { id } = useParams();
     const [producto, setProducto] = useState({})
     useEffect(() => {
-        axios.get("http://localhost:8000/api/producto/" + props.id)
+        axios.get("http://localhost:8000/api/producto/" + id)
             .then(res => setProducto({
                 ...res.data
             }))
@@ -14,6 +17,7 @@ export default props => {
             <p>Titulo: {producto.titulo}</p>
             <p>Precio: {producto.precio}</p>
             <p>Descripcion: {producto.descripcion}</p>
+            <a className='btn btn-outline-secondary' href="/">Go Back</a>
         </div>
     )
 }
